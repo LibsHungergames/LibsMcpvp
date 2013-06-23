00645016dcf97f557f5dff09c37d8cad307828f5
Andrew <libraryaddict115@yahoo.co.nz>
2013-06-24 00:28:54 +1200

    Released 2.3

--- src/me/libraryaddict/librarys/Abilities/JackHammer.java
+++ src/me/libraryaddict/librarys/Abilities/JackHammer.java
@@ -2,7 +2,6 @@ package me.libraryaddict.librarys.Abilities;
 
 import java.util.HashMap;
 
-import org.bukkit.Bukkit;
 import org.bukkit.ChatColor;
 import org.bukkit.Material;
 import org.bukkit.block.Block;
