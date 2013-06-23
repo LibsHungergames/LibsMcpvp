c117f4212629a35ee6ad8cd3d83f7e5ed2cd4fcd
Andrew <libraryaddict115@yahoo.co.nz>
2013-06-24 00:26:47 +1200

    Worm now lands on dirt safely

--- src/me/libraryaddict/librarys/Abilities/Worm.java
+++ src/me/libraryaddict/librarys/Abilities/Worm.java
@@ -1,16 +1,21 @@
 package me.libraryaddict.librarys.Abilities;
 
 import org.bukkit.Effect;
+import org.bukkit.Location;
 import org.bukkit.Material;
+import org.bukkit.block.Block;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.block.BlockDamageEvent;
+import org.bukkit.event.entity.EntityDamageEvent;
+import org.bukkit.event.entity.EntityDamageEvent.DamageCause;
 import org.bukkit.inventory.ItemStack;
 
 import me.libraryaddict.Hungergames.Interfaces.Disableable;
 import me.libraryaddict.Hungergames.Types.AbilityListener;
 
 public class Worm extends AbilityListener implements Disableable {
+    public boolean dirtBlocksDamage = true;
 
     @EventHandler
     public void onDamage(BlockDamageEvent event) {
@@ -32,4 +37,22 @@ public class Worm extends AbilityListener implements Disableable {
         }
     }
 
+    @EventHandler
+    public void onDamage(EntityDamageEvent event) {
+        if (dirtBlocksDamage && event.getCause() == DamageCause.FALL && event.getEntity() instanceof Player) {
+            if (hasAbility((Player) event.getEntity())) {
+                Location loc = event.getEntity().getLocation();
+                boolean dirt = false;
+                for (float x = -0.35F; x <= 0.35F && !dirt; x += 0.35F) {
+                    for (float z = -0.35F; z <= 0.35F && !dirt; z += 0.35F) {
+                        Block b = loc.clone().add(x, -1, z).getBlock();
+                        if (b.getType() == Material.DIRT)
+                            dirt = true;
+                    }
+                }
+                if (dirt)
+                    event.setCancelled(true);
+            }
+        }
+    }
 }
