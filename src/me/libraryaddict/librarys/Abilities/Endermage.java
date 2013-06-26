9a1197f26d88b3ef13d68eed670f92fb20e4dce4
Andrew <libraryaddict115@yahoo.co.nz>
2013-06-26 20:31:24 +1200

    Endermage now reflects the latest MCPVP endermage

--- src/me/libraryaddict/librarys/Abilities/Endermage.java
+++ src/me/libraryaddict/librarys/Abilities/Endermage.java
@@ -1,109 +1,73 @@
 package me.libraryaddict.librarys.Abilities;
 
-import java.util.ArrayList;
+import java.util.HashMap;
 
+import me.libraryaddict.Hungergames.Hungergames;
 import me.libraryaddict.Hungergames.Interfaces.Disableable;
-import me.libraryaddict.Hungergames.Managers.EnchantmentManager;
 import me.libraryaddict.Hungergames.Types.AbilityListener;
 import me.libraryaddict.Hungergames.Types.HungergamesApi;
 import me.libraryaddict.Hungergames.Types.Gamer;
 
-import org.bukkit.Bukkit;
 import org.bukkit.ChatColor;
 import org.bukkit.Effect;
 import org.bukkit.Location;
-import org.bukkit.Material;
 import org.bukkit.Sound;
 import org.bukkit.block.Block;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.block.BlockPlaceEvent;
-import org.bukkit.event.entity.EntityDamageByEntityEvent;
 import org.bukkit.inventory.ItemStack;
-import org.bukkit.inventory.meta.ItemMeta;
-import org.bukkit.metadata.FixedMetadataValue;
 
 public class Endermage extends AbilityListener implements Disableable {
-    public boolean doInstantKO = true;
-    public int endermagePortalBlockId = Material.ENDER_PORTAL.getId();
-    public int endermagePortalId = Material.ENDER_PORTAL.getId();
     public String endermagePortalName = "Endermage Portal";
-    private transient ArrayList<Block> endermages = new ArrayList<Block>();
-    public int instanceKOExpires = 5;
+    private transient HashMap<Player, Integer> cooldown = new HashMap<Player, Integer>();
+    public int cooldownTime = 30;
     public int invincibleTicks = 100;
+    private Hungergames hg = HungergamesApi.getHungergames();
+    public String cooldownMessage = ChatColor.RED + "You cannot use this just yet! Wait %s seconds!";
+    public String nooneWarped = ChatColor.RED + "You didn't get anyone! Cooldown still applies!";
+    public String warpedMessage = ChatColor.LIGHT_PURPLE + "Warped!\nYou are invincible for 5 seconds!\nPrepare to fight!";
 
     private boolean isEnderable(Location portal, Location player) {
         return Math.abs(portal.getX() - player.getX()) < 2 && Math.abs(portal.getZ() - player.getZ()) < 2
                 && Math.abs(portal.getY() - player.getY()) >= 3.5;
     }
 
-    @EventHandler
-    public void onEntityDamageByEntity(EntityDamageByEntityEvent event) {
-        if (doInstantKO)
-            if (event.getEntity() instanceof Player
-                    && event.getDamager().hasMetadata("InstantKill" + ((Player) event.getEntity()).getName())
-                    && event.getDamager().getMetadata("InstantKill" + ((Player) event.getEntity()).getName()).get(0).asLong() < System
-                            .currentTimeMillis())
-                event.setDamage(9999);
+    private void warp(Player victim, Location portal) {
+        victim.playEffect(victim.getLocation(), Effect.ENDER_SIGNAL, 9);
+        victim.playEffect(portal, Effect.ENDER_SIGNAL, 9);
+        victim.playSound(victim.getLocation(), Sound.ENDERMAN_TELEPORT, 1, 1.2F);
+        victim.playSound(portal, Sound.ENDERMAN_TELEPORT, 1, 1.2F);
+        if (invincibleTicks > 0)
+            victim.setNoDamageTicks(invincibleTicks);
+        victim.teleport(portal);
+        victim.sendMessage(warpedMessage);
     }
 
     @EventHandler
     public void onPlace(BlockPlaceEvent event) {
         ItemStack item = event.getItemInHand();
-        if (item != null && item.getTypeId() == endermagePortalId && isSpecialItem(item, endermagePortalName)
-                && hasAbility(event.getPlayer())) {
+        if (isSpecialItem(item, endermagePortalName) && hasAbility(event.getPlayer())) {
             event.setCancelled(true);
+            Player mage = event.getPlayer();
             final Block b = event.getBlock();
-            if (endermages.contains(b))
-                return;
-            endermages.add(b);
-            item.setAmount(item.getAmount() - 1);
-            if (item.getAmount() == 0)
-                event.getPlayer().setItemInHand(new ItemStack(0));
-            final Location portal = b.getLocation().clone().add(0.5, 0.5, 0.5);
-            portal.getBlock().setTypeId(endermagePortalBlockId);
-            final Gamer mager = HungergamesApi.getPlayerManager().getGamer(event.getPlayer());
-            for (int i = 0; i <= 5; i++) {
-                final int no = i;
-                Bukkit.getScheduler().scheduleSyncDelayedTask(HungergamesApi.getHungergames(), new Runnable() {
-                    public void run() {
-                        for (Gamer gamer : HungergamesApi.getPlayerManager().getAliveGamers()) {
-                            Player p = gamer.getPlayer();
-                            if (p != mager.getPlayer() && isEnderable(portal, p.getLocation())) {
-                                if (doInstantKO)
-                                    p.setMetadata("InstantKill" + mager.getName(),
-                                            new FixedMetadataValue(HungergamesApi.getHungergames(), System.currentTimeMillis()
-                                                    + (instanceKOExpires * 1000)));
-                                if (gamer.isAlive()) {
-                                    if (p.getLocation().distance(portal) > 4) {
-                                        p.playEffect(p.getLocation(), Effect.ENDER_SIGNAL, 9);
-                                        p.playEffect(portal, Effect.ENDER_SIGNAL, 9);
-                                        p.playSound(p.getLocation(), Sound.ENDERMAN_TELEPORT, 1, 1.2F);
-                                        p.playSound(portal, Sound.ENDERMAN_TELEPORT, 1, 1.2F);
-                                    }
-                                    if (invincibleTicks > 0)
-                                        p.setNoDamageTicks(invincibleTicks);
-                                    p.teleport(portal.clone().add(0, -0.5, 0));
-                                    p.setFallDistance(-100);
-                                }
-                            }
-                        }
-                        if (no == 5) {
-                            if (mager.isAlive()) {
-                                ItemStack item = new ItemStack(endermagePortalId);
-                                ItemMeta meta = item.getItemMeta();
-                                if (endermagePortalName.equals(ChatColor.stripColor(endermagePortalName)))
-                                    endermagePortalName = ChatColor.WHITE + endermagePortalName;
-                                meta.setDisplayName(endermagePortalName);
-                                item.setItemMeta(meta);
-                                item.addEnchantment(EnchantmentManager.UNLOOTABLE, 1);
-                                EnchantmentManager.updateEnchants(item);
-                                HungergamesApi.getKitManager().addItem(mager.getPlayer(), item);
-                                endermages.remove(b);
-                            }
-                        }
+            if (cooldown.containsKey(mage) && cooldown.get(mage) > hg.currentTime) {
+                mage.sendMessage(String.format(cooldownMessage, cooldown.get(mage) - hg.currentTime));
+            } else {
+                Location portal = b.getLocation().clone().add(0.5, 0.5, 0.5);
+                boolean foundSomeone = false;
+                for (Gamer gamer : HungergamesApi.getPlayerManager().getAliveGamers()) {
+                    Player victim = gamer.getPlayer();
+                    if (gamer.isAlive() && victim != mage && isEnderable(portal, victim.getLocation())) {
+                        foundSomeone = true;
+                        warp(victim, portal);
                     }
-                }, i * 20);
+                }
+                if (foundSomeone) {
+                    warp(mage, portal);
+                    cooldown.put(mage, hg.currentTime + cooldownTime);
+                } else
+                    mage.sendMessage(nooneWarped);
             }
         }
     }
