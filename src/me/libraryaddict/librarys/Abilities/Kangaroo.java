c050abcc15dc3049067513f52e82b06abd2d8005
Andrew <libraryaddict115@yahoo.co.nz>
2013-06-27 02:26:39 +1200

    Fixed jump height

--- src/me/libraryaddict/librarys/Abilities/Kangaroo.java
+++ src/me/libraryaddict/librarys/Abilities/Kangaroo.java
@@ -13,19 +13,20 @@ import me.libraryaddict.Hungergames.Interfaces.Disableable;
 import me.libraryaddict.Hungergames.Types.AbilityListener;
 
 public class Kangaroo extends AbilityListener implements Disableable {
-    public float jumpHeight = 2F;
+    public float jumpHeight = 4F;
     public String kangarooBootsName = "Kangaroo Boots";
 
     @EventHandler
     public void onInteract(PlayerInteractEvent event) {
         Player p = event.getPlayer();
         if (event.getAction() != Action.PHYSICAL && hasAbility(p) && isSpecialItem(event.getItem(), kangarooBootsName)) {
+            event.setCancelled(true);
             Block b = p.getLocation().getBlock();
             if (b.getType() != Material.AIR || b.getRelative(BlockFace.DOWN).getType() != Material.AIR) {
                 p.setFallDistance(-(jumpHeight + 1));
                 Vector vector = p.getEyeLocation().getDirection();
                 vector.multiply(0.6F);
-                vector.setY(jumpHeight / 4);
+                vector.setY(jumpHeight / 4F);
                 p.setVelocity(vector);
             }
         }
