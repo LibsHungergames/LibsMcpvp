20579d182d65ca9ee8bcbf63bd2d7fc59f1cb302
Andrew <libraryaddict115@yahoo.co.nz>
2013-07-19 05:24:55 +1200

    Untested thingy for berserker to be balanced again

--- src/me/libraryaddict/librarys/Abilities/Berserker.java
+++ src/me/libraryaddict/librarys/Abilities/Berserker.java
@@ -1,9 +1,14 @@
 package me.libraryaddict.librarys.Abilities;
 
+import java.util.HashMap;
+
 import me.libraryaddict.Hungergames.Types.AbilityListener;
+import me.libraryaddict.Hungergames.Types.HungergamesApi;
+
 import org.bukkit.entity.Creature;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
+import org.bukkit.event.entity.EntityDamageByEntityEvent;
 import org.bukkit.event.entity.EntityDeathEvent;
 import org.bukkit.potion.PotionEffect;
 import org.bukkit.potion.PotionEffectType;
@@ -12,33 +17,55 @@ import me.libraryaddict.Hungergames.Events.PlayerKilledEvent;
 import me.libraryaddict.Hungergames.Interfaces.Disableable;
 
 public class Berserker extends AbilityListener implements Disableable {
+    private class DmgBoost {
+        int expires;
+        int extraDamage;
+    }
+
     public int berserkerLength = 30;
+    private HashMap<Player, DmgBoost> dmgBuff = new HashMap<Player, DmgBoost>();
+    public int extraDamageMob = 2;
+    public int extraDamagePlayer = 4;
     public boolean giveConfusion = true;
-    public int killAnimalMultiplier = 0;
-    public int killPlayerMultiplier = 1;
+
+    @EventHandler
+    public void onDamageByEntity(EntityDamageByEntityEvent event) {
+        if (dmgBuff.containsKey(event.getDamager())) {
+            DmgBoost boost = dmgBuff.get(event.getDamager());
+            if (boost.expires < HungergamesApi.getHungergames().currentTime)
+                dmgBuff.remove(event.getDamager());
+            else
+                event.setDamage(event.getDamage() + boost.extraDamage);
+        }
+    }
 
     @EventHandler
     public void onDeath(EntityDeathEvent event) {
         if (event.getEntity().getKiller() != null && hasAbility(event.getEntity().getKiller())) {
             Player p = event.getEntity().getKiller();
             if (event.getEntity() instanceof Creature) {
-                p.addPotionEffect(new PotionEffect(PotionEffectType.INCREASE_DAMAGE, berserkerLength * 20, killAnimalMultiplier),
-                        true);
-                if (giveConfusion)
-                    p.addPotionEffect(new PotionEffect(PotionEffectType.CONFUSION, berserkerLength * 20, 0), true);
+                setDamage(p, extraDamageMob);
             }
         }
     }
 
     @EventHandler
     public void onDeath(PlayerKilledEvent event) {
-        if (event.getKillerPlayer() != null && hasAbility(event.getKillerPlayer().getPlayer())) {
+        dmgBuff.remove(event.getKilled().getPlayer());
+        if (event.getKillerPlayer() != null) {
             Player p = event.getKillerPlayer().getPlayer();
-            p.addPotionEffect(new PotionEffect(PotionEffectType.INCREASE_DAMAGE, berserkerLength * 20, killPlayerMultiplier),
-                    true);
-            if (giveConfusion)
-                p.addPotionEffect(new PotionEffect(PotionEffectType.CONFUSION, berserkerLength * 20, 1), true);
+            if (hasAbility(p))
+                setDamage(p, extraDamagePlayer);
         }
+    }
+
+    private void setDamage(Player p, int damage) {
+        DmgBoost boost = new DmgBoost();
+        boost.expires = HungergamesApi.getHungergames().currentTime + berserkerLength;
+        boost.extraDamage = damage;
+        dmgBuff.put(p, boost);
+        if (giveConfusion)
+            p.addPotionEffect(new PotionEffect(PotionEffectType.CONFUSION, berserkerLength * 20, 0), true);
     }
 
 }
