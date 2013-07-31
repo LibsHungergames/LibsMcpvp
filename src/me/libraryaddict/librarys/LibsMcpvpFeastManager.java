c215b3d2a4fb72e72c1ca81fea940f28fec64dff
Andrew <libraryaddict115@yahoo.co.nz>
2013-07-31 16:31:00 +1200

    Feast fixupitys

--- src/me/libraryaddict/librarys/LibsMcpvpFeastManager.java
+++ src/me/libraryaddict/librarys/LibsMcpvpFeastManager.java
@@ -1,43 +1,47 @@
 package me.libraryaddict.librarys;
 
+import java.util.LinkedList;
+
 import org.bukkit.Location;
 import org.bukkit.Material;
 import org.bukkit.block.Block;
 import org.bukkit.block.Chest;
+import org.bukkit.scheduler.BukkitRunnable;
+
 import me.libraryaddict.Hungergames.Interfaces.ChestManager;
 import me.libraryaddict.Hungergames.Managers.LibsFeastManager;
 import me.libraryaddict.Hungergames.Types.HungergamesApi;
 
 public class LibsMcpvpFeastManager extends LibsFeastManager {
-    public void generateChests(Location loc, int height) {
-        height--;
-        ChestManager cm = HungergamesApi.getChestManager();
-        for (int x = -height; x <= height; x++) {
-            for (int z = -height; z <= height; z++) {
-                Block block = loc.clone().add(x, 0, z).getBlock();
-                Block b = block;
-                if (x == 0 && z == 0)
-                    setBlockFast(b, Material.ENCHANTMENT_TABLE.getId(), (short) 0);
-                else if (Math.abs(x + z) % 2 == 0) {
-                    setBlockFast(block, Material.CHEST.getId(), (byte) 0);
-                    Chest chest = (Chest) block.getState();
-                    cm.fillChest(chest.getInventory());
-                    chest.update();
+    private BukkitRunnable chestGenerator;
+    private BukkitRunnable runnable;
+    private LinkedList<Block> processedBlocks = new LinkedList<Block>();
+
+    public void generateChests(final Location loc, int height) {
+        final int h = height - 1;
+        chestGenerator = new BukkitRunnable() {
+            public void run() {
+                ChestManager cm = HungergamesApi.getChestManager();
+                for (int x = -h; x <= h; x++) {
+                    for (int z = -h; z <= h; z++) {
+                        Block block = loc.clone().add(x, 0, z).getBlock();
+                        Block b = block;
+                        if (x == 0 && z == 0)
+                            setBlockFast(b, Material.ENCHANTMENT_TABLE.getId(), (short) 0);
+                        else if (Math.abs(x + z) % 2 == 0) {
+                            block.setTypeIdAndData(Material.CHEST.getId(), (byte) 0, false);
+                            processedBlocks.add(block);
+                            Chest chest = (Chest) block.getState();
+                            cm.fillChest(chest.getInventory());
+                            chest.update();
+                        }
+                    }
                 }
             }
+        };
+        if (runnable == null) {
+            chestGenerator.runTask(HungergamesApi.getHungergames());
+            chestGenerator = null;
         }
-    }
-
-    private boolean setBlockFast(Block b, int typeId, short s) {
-        try {
-            if (b.getTypeId() != typeId || b.getData() != s)
-                return b.setTypeIdAndData(typeId, (byte) s, false);
-            // return ((CraftChunk) b.getChunk()).getHandle().a(b.getX() & 15,
-            // b.getY(), b.getZ() & 15, typeId, data);
-        } catch (Exception ex) {
-            ex.printStackTrace();
-            return false;
-        }
-        return true;
     }
 }
