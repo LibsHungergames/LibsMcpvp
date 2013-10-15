e59f56f678e499ffe9250f7a3548a57aaf3032a6
Andrew <libraryaddict115@yahoo.co.nz>
2013-10-15 21:33:33 +1300

    Werewolf should no longer be as bugged

--- src/me/libraryaddict/librarys/Abilities/Werewolf.java
+++ src/me/libraryaddict/librarys/Abilities/Werewolf.java
@@ -4,6 +4,7 @@ import me.libraryaddict.Hungergames.Events.GameStartEvent;
 import me.libraryaddict.Hungergames.Interfaces.Disableable;
 import me.libraryaddict.Hungergames.Types.AbilityListener;
 import org.bukkit.Bukkit;
+import org.bukkit.World;
 import org.bukkit.entity.EntityType;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
@@ -14,32 +15,35 @@ import org.bukkit.potion.PotionEffectType;
 import me.libraryaddict.Hungergames.Types.HungergamesApi;
 
 public class Werewolf extends AbilityListener implements Disableable {
-    public String[] potionEffectsDay = new String[] { "WEAKNESS 12000 0" };
-    public String[] potionEffectsNight = new String[] { "SPEED 12000 0", "INCREASE_DAMAGE 12000 0" };
+    public String[] potionEffectsDuringDay = new String[] { "WEAKNESS 0" };
+    public String[] potionEffectsDuringNight = new String[] { "SPEED 0", "INCREASE_DAMAGE 0" };
     private int scheduler = -1;
 
     @EventHandler
     public void gameStartEvent(GameStartEvent event) {
-        scheduler = Bukkit.getScheduler().scheduleSyncRepeatingTask(HungergamesApi.getHungergames(), getRunnable(), 0, 12000);
+        getRunnable().run();
+        scheduler = Bukkit.getScheduler().scheduleSyncRepeatingTask(HungergamesApi.getHungergames(), getRunnable(),
+                12000 - (HungergamesApi.getHungergames().world.getTime() % 12000), 12000);
     }
 
     private Runnable getRunnable() {
         return new Runnable() {
             public void run() {
+                World world = HungergamesApi.getHungergames().world;
                 for (Player p : getMyPlayers()) {
-                    if (HungergamesApi.getHungergames().world.getTime() > 0
-                            && HungergamesApi.getHungergames().world.getTime() <= 12000) {
-                        for (String string : potionEffectsDay) {
+                    if (HungergamesApi.getHungergames().world.getTime() >= 0
+                            && HungergamesApi.getHungergames().world.getTime() < 12000) {
+                        for (String string : potionEffectsDuringDay) {
                             String[] effect = string.split(" ");
                             PotionEffect potionEffect = new PotionEffect(PotionEffectType.getByName(effect[0].toUpperCase()),
-                                    Integer.parseInt(effect[1]), Integer.parseInt(effect[2]));
+                                    (int) (12000 - (world.getTime() % 12000)), Integer.parseInt(effect[1]));
                             p.addPotionEffect(potionEffect, true);
                         }
                     } else {
-                        for (String string : potionEffectsNight) {
+                        for (String string : potionEffectsDuringNight) {
                             String[] effect = string.split(" ");
                             PotionEffect potionEffect = new PotionEffect(PotionEffectType.getByName(effect[0].toUpperCase()),
-                                    Integer.parseInt(effect[1]), Integer.parseInt(effect[2]));
+                                    (int) (12000 - (world.getTime() % 12000)), Integer.parseInt(effect[1]));
                             p.addPotionEffect(potionEffect, true);
                         }
                     }
