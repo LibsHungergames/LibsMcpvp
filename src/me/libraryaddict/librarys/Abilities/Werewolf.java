719ab07e2574f4efe0272370bdfbebcdf5b973dd
Andrew <libraryaddict115@yahoo.co.nz>
2013-10-15 21:58:15 +1300

    Released 3.1

--- src/me/libraryaddict/librarys/Abilities/Werewolf.java
+++ src/me/libraryaddict/librarys/Abilities/Werewolf.java
@@ -1,19 +1,6 @@
 package me.libraryaddict.librarys.Abilities;
 
 import me.libraryaddict.Hungergames.Abilities.Ninja;
-import me.libraryaddict.Hungergames.Events.GameStartEvent;
-import me.libraryaddict.Hungergames.Interfaces.Disableable;
-import me.libraryaddict.Hungergames.Types.AbilityListener;
-import org.bukkit.Bukkit;
-import org.bukkit.World;
-import org.bukkit.entity.EntityType;
-import org.bukkit.entity.Player;
-import org.bukkit.event.EventHandler;
-import org.bukkit.event.entity.EntityTargetEvent;
-import org.bukkit.potion.PotionEffect;
-import org.bukkit.potion.PotionEffectType;
-
-import me.libraryaddict.Hungergames.Types.HungergamesApi;
 
 public class Werewolf extends Ninja {
     public String[] potionEffectsDuringDay = new String[] { "WEAKNESS 0" };
