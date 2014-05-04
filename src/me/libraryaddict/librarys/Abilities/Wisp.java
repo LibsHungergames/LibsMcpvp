fc2d697ad5250a253dcb91e4a9ef349dbdbc76b0
libraryaddict <libraryaddict115@yahoo.co.nz>
2014-05-05 01:37:55 +1200

    Use disguised ocelot instead of a villager and nms

--- src/me/libraryaddict/librarys/Abilities/Wisp.java
+++ src/me/libraryaddict/librarys/Abilities/Wisp.java
@@ -1,38 +1,23 @@
 package me.libraryaddict.librarys.Abilities;
 
-import java.lang.reflect.Field;
 import java.util.ArrayList;
 import java.util.Iterator;
-import java.util.List;
-
-import net.minecraft.server.v1_7_R3.EntityHuman;
-import net.minecraft.server.v1_7_R3.EntityInsentient;
-import net.minecraft.server.v1_7_R3.EntityVillager;
-import net.minecraft.server.v1_7_R3.PathfinderGoal;
-import net.minecraft.server.v1_7_R3.PathfinderGoalAvoidPlayer;
-import net.minecraft.server.v1_7_R3.PathfinderGoalFloat;
-import net.minecraft.server.v1_7_R3.PathfinderGoalLookAtPlayer;
-import net.minecraft.server.v1_7_R3.PathfinderGoalRandomLookaround;
-import net.minecraft.server.v1_7_R3.PathfinderGoalRandomStroll;
-import net.minecraft.server.v1_7_R3.PathfinderGoalSelector;
-
 import org.bukkit.Bukkit;
 import org.bukkit.ChatColor;
 import org.bukkit.Effect;
 import org.bukkit.Location;
 import org.bukkit.Sound;
-import org.bukkit.craftbukkit.v1_7_R3.CraftServer;
-import org.bukkit.craftbukkit.v1_7_R3.entity.CraftVillager;
 import org.bukkit.entity.Entity;
 import org.bukkit.entity.EntityType;
+import org.bukkit.entity.Ocelot;
 import org.bukkit.entity.Player;
-import org.bukkit.entity.Villager;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.entity.EntityDamageByEntityEvent;
 import org.bukkit.event.entity.EntityDamageEvent;
 import org.bukkit.event.player.PlayerInteractEntityEvent;
 import org.bukkit.event.player.PlayerInteractEvent;
 import org.bukkit.inventory.ItemStack;
+
 import me.libraryaddict.Hungergames.Events.PlayerKilledEvent;
 import me.libraryaddict.Hungergames.Interfaces.Disableable;
 import me.libraryaddict.Hungergames.Types.AbilityListener;
@@ -44,37 +29,13 @@ public class Wisp extends AbilityListener implements Disableable {
 
     class WillOfWisp {
         Player caster;
-        ArrayList<Villager> villagers = new ArrayList<Villager>();
+        ArrayList<Ocelot> villagers = new ArrayList<Ocelot>();
     }
 
-    public static Villager spawnVillager(Location loc) {
-        Villager villager = (Villager) loc.getWorld().spawnEntity(loc, EntityType.VILLAGER);
+    public static Ocelot spawnVillager(Location loc) {
+        Ocelot villager = (Ocelot) loc.getWorld().spawnEntity(loc, EntityType.OCELOT);
         villager.setNoDamageTicks(Integer.MAX_VALUE);
         villager.setBreed(false);
-        try {
-            EntityVillager ent = ((CraftVillager) villager).getHandle();
-
-            Field goalField = EntityInsentient.class.getDeclaredField("goalSelector");
-            goalField.setAccessible(true);
-            PathfinderGoalSelector goalSelector = (PathfinderGoalSelector) goalField.get(ent);
-
-            Field targetField = EntityInsentient.class.getDeclaredField("targetSelector");
-            targetField.setAccessible(true);
-
-            Field aField = PathfinderGoalSelector.class.getDeclaredField("b");
-            aField.setAccessible(true);
-            ((List<PathfinderGoal>) aField.get(goalSelector)).clear();
-            ((List<PathfinderGoal>) aField.get((PathfinderGoalSelector) targetField.get(ent))).clear();
-
-            goalSelector.a(0, new PathfinderGoalFloat(ent));
-            goalSelector.a(1, new PathfinderGoalAvoidPlayer(ent, EntityHuman.class, 6.0F, 0.35F, 0.4F));
-            goalSelector.a(2, new PathfinderGoalAvoidPlayer(ent, EntityVillager.class, 6.0F, 0.35F, 0.4F));
-            goalSelector.a(3, new PathfinderGoalRandomStroll(ent, 0.35F));
-            goalSelector.a(4, new PathfinderGoalLookAtPlayer(ent, EntityHuman.class, 6.0F));
-            goalSelector.a(5, new PathfinderGoalRandomLookaround(ent));
-        } catch (Exception ex) {
-            ex.printStackTrace();
-        }
         return villager;
     }
 
@@ -98,8 +59,6 @@ public class Wisp extends AbilityListener implements Disableable {
         if (Bukkit.getPluginManager().getPlugin("ProtocolLib") == null)
             throw new Exception(String.format(HungergamesApi.getConfigManager().getLoggerConfig().getDependencyNotFound(),
                     "Plugin ProtocolLib"));
-        if (!((CraftServer) Bukkit.getServer()).getServer().getPropertyManager().getBoolean("spawn-npcs", false))
-            throw new Exception("NPC's in server.properties is disabled, enable this to use the kit Wisp");
     }
 
     @EventHandler
@@ -112,13 +71,13 @@ public class Wisp extends AbilityListener implements Disableable {
             if (wisp.caster == event.getEntity()) {
                 Player p = (Player) event.getEntity();
                 p.sendMessage(allWispsPopped);
-                for (Villager villager : wisp.villagers)
+                for (Ocelot villager : wisp.villagers)
                     popWisp(villager);
                 itel.remove();
                 break;
             } else if (wisp.villagers.contains(event.getEntity())) {
                 event.setCancelled(true);
-                popWisp((Villager) event.getEntity());
+                popWisp((Ocelot) event.getEntity());
                 wisp.villagers.remove(event.getEntity());
                 if (wisp.villagers.size() == 0) {
                     wisp.caster.sendMessage(allWispsPopped);
@@ -147,25 +106,18 @@ public class Wisp extends AbilityListener implements Disableable {
             wisp.caster = p;
             p.getWorld().playSound(p.getLocation(), Sound.BAT_TAKEOFF, 1, 0);
             for (int i = 0; i < wispsToSpawn; i++) {
-                Villager villager = spawnVillager(p.getLocation());
+                Ocelot villager = spawnVillager(p.getLocation());
                 wisp.villagers.add(villager);
                 PlayerDisguise player = new PlayerDisguise(p.getName());
+                player.getWatcher().setItemInHand(p.getItemInHand());
+                player.getWatcher().setArmor(p.getInventory().getArmorContents());
                 DisguiseAPI.disguiseToAll(villager, player);
                 villager.setRemoveWhenFarAway(true);
-                villager.getEquipment().setArmorContents(p.getInventory().getArmorContents());
-                if (p.getItemInHand() != null) {
-                    villager.getEquipment().setItemInHand(p.getItemInHand());
-                    villager.getEquipment().setItemInHandDropChance(0F);
-                }
-                villager.getEquipment().setBootsDropChance(0F);
-                villager.getEquipment().setLeggingsDropChance(0F);
-                villager.getEquipment().setChestplateDropChance(0F);
-                villager.getEquipment().setHelmetDropChance(0F);
             }
             wisps.add(wisp);
             Bukkit.getScheduler().scheduleSyncDelayedTask(HungergamesApi.getHungergames(), new Runnable() {
                 public void run() {
-                    for (Villager villager : wisp.villagers)
+                    for (Ocelot villager : wisp.villagers)
                         popWisp(villager);
                     wisps.remove(wisp);
                 }
@@ -186,14 +138,14 @@ public class Wisp extends AbilityListener implements Disableable {
         while (itel.hasNext()) {
             WillOfWisp wisp = itel.next();
             if (wisp.caster == event.getKilled().getPlayer()) {
-                for (Villager villager : wisp.villagers)
+                for (Ocelot villager : wisp.villagers)
                     popWisp(villager);
                 itel.remove();
             }
         }
     }
 
-    private void popWisp(Villager villager) {
+    private void popWisp(Ocelot villager) {
         Location loc = villager.getLocation().clone();
         loc.getWorld().playSound(loc, Sound.FIZZ, 2, 0);
         for (int i = 0; i <= 9; i++)
