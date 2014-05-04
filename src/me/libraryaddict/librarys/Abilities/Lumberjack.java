0561fb1730180267c4260e14661bf11687d8c2fe
libraryaddict <libraryaddict115@yahoo.co.nz>
2014-05-05 01:13:33 +1200

    Allow lumberjack items to be set. Also fixes Log_2 not existing in older MC

--- src/me/libraryaddict/librarys/Abilities/Lumberjack.java
+++ src/me/libraryaddict/librarys/Abilities/Lumberjack.java
@@ -3,22 +3,34 @@ package me.libraryaddict.librarys.Abilities;
 import me.libraryaddict.Hungergames.Interfaces.Disableable;
 import me.libraryaddict.Hungergames.Types.AbilityListener;
 
-import org.bukkit.Material;
 import org.bukkit.block.Block;
 import org.bukkit.block.BlockFace;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.block.BlockBreakEvent;
 
 public class Lumberjack extends AbilityListener implements Disableable {
+    public String[] materialsEffected = new String[] { "LOG", "LOG_2" };
 
     @EventHandler
     public void onBreak(BlockBreakEvent event) {
-        if ((event.getBlock().getType() == Material.LOG || event.getBlock().getType() == Material.LOG_2)
-                && hasAbility(event.getPlayer())) {
-            Block b = event.getBlock().getRelative(BlockFace.UP);
-            while (b.getType() == Material.LOG || b.getType() == Material.LOG_2) {
-                b.breakNaturally();
-                b = b.getRelative(BlockFace.UP);
+        if (hasAbility(event.getPlayer())) {
+            for (String mat : materialsEffected) {
+                if (event.getBlock().getType().name().equalsIgnoreCase(mat)) {
+                    Block b = event.getBlock().getRelative(BlockFace.UP);
+                    boolean hasUp = true;
+                    while (hasUp) {
+                        hasUp = false;
+                        for (String type : materialsEffected) {
+                            if (b.getType().name().equalsIgnoreCase(type)) {
+                                b.breakNaturally();
+                                b = b.getRelative(BlockFace.UP);
+                                hasUp = true;
+                                break;
+                            }
+                        }
+                    }
+                    break;
+                }
             }
         }
     }
