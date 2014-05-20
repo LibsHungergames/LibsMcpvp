94efbf721248b8c49e335d08046a9c533129e764
libraryaddict <libraryaddict115@yahoo.co.nz>
2014-05-21 01:50:22 +1200

    Make barbarian do lore for kills

--- src/me/libraryaddict/librarys/Abilities/Barbarian.java
+++ src/me/libraryaddict/librarys/Abilities/Barbarian.java
@@ -1,30 +1,49 @@
 package me.libraryaddict.librarys.Abilities;
 
-import java.util.HashMap;
+import java.util.Arrays;
+import java.util.List;
 
 import org.bukkit.Material;
 import org.bukkit.event.EventHandler;
 import org.bukkit.inventory.ItemStack;
+import org.bukkit.inventory.meta.ItemMeta;
 
 import me.libraryaddict.Hungergames.Events.PlayerKilledEvent;
 import me.libraryaddict.Hungergames.Types.AbilityListener;
 
 public class Barbarian extends AbilityListener {
-    private HashMap<ItemStack, Integer> kills = new HashMap<ItemStack, Integer>();
     public int killsPerLevel = 3;
     public String swordName = "Bloody Bane";
     private Material[] updates = new Material[] { Material.STONE_SWORD, Material.IRON_SWORD, Material.DIAMOND_SWORD };
+    public String swordLore = "%Kills% kills";
+
+    public Barbarian() {
+        if (!swordLore.contains("%Kills%")) {
+            swordLore = "%Kills% kills";
+        }
+    }
 
     @EventHandler
     public void onKilled(PlayerKilledEvent event) {
         if (event.getKillerPlayer() != null) {
             ItemStack item = event.getKillerPlayer().getPlayer().getItemInHand();
             if (isSpecialItem(item, swordName) && hasAbility(event.getKillerPlayer().getPlayer())) {
-                if (!kills.containsKey(item))
-                    kills.put(item, 0);
-                kills.put(item, kills.get(item) + 1);
-                if (kills.get(item) % 3 == 0) {
-                    int level = (kills.get(item) / 3) - 1;
+                ItemMeta meta = item.getItemMeta();
+                int kills = 0;
+                if (meta.hasLore()) {
+                    List<String> lore = meta.getLore();
+                    if (lore.size() > 0) {
+                        String str = lore.get(0);
+                        str = str.substring(swordLore.indexOf("%Kills%"));
+                        str = str.substring(0, str.length() - (swordLore.length() - swordLore.lastIndexOf("%Kills%")));
+                        kills = Integer.parseInt(str);
+                    }
+                }
+                kills++;
+                meta.setLore(Arrays.asList(swordLore.replace("%Kills%", "" + kills)));
+                item.setItemMeta(meta);
+                if (kills % killsPerLevel == 0) {
+                    int level = kills / killsPerLevel;
                     if (level < updates.length)
                         item.setType(updates[level]);
                 }
