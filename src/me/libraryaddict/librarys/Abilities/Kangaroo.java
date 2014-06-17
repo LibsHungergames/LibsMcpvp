d01a05525231e7da95b27ebfde93cffd14db81b2
libraryaddict <libraryaddict115@yahoo.co.nz>
2014-06-18 04:14:38 +1200

    Added new config option for kangaroo

--- src/me/libraryaddict/librarys/Abilities/Kangaroo.java
+++ src/me/libraryaddict/librarys/Abilities/Kangaroo.java
@@ -14,6 +14,7 @@ import me.libraryaddict.Hungergames.Types.AbilityListener;
 public class Kangaroo extends AbilityListener implements Disableable {
     public float jumpHeight = 4F;
     public String kangarooBootsName = "Kangaroo Fireworks";
+    public boolean preventFallDamageEntirely = false;
 
     @EventHandler
     public void onInteract(PlayerInteractEvent event) {
@@ -22,7 +23,11 @@ public class Kangaroo extends AbilityListener implements Disableable {
             event.setCancelled(true);
             Block b = p.getLocation().getBlock();
             if (b.getType() != Material.AIR || b.getRelative(BlockFace.DOWN).getType() != Material.AIR) {
-                p.setFallDistance(-(jumpHeight + 1));
+                if (preventFallDamageEntirely) {
+                    p.setFallDistance(-99999);
+                } else {
+                    p.setFallDistance(-(jumpHeight + 1));
+                }
                 Vector vector = p.getEyeLocation().getDirection();
                 vector.multiply(0.6F);
                 vector.setY(jumpHeight / 4F);
