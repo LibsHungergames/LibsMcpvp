90b5ff6776c6e29a7e633f2ffb64df2c0136b537
libraryaddict <libraryaddict115@yahoo.co.nz>
2014-08-11 16:04:48 +1200

    Werewolf should now work properly

--- src/me/libraryaddict/librarys/Abilities/Werewolf.java
+++ src/me/libraryaddict/librarys/Abilities/Werewolf.java
@@ -1,8 +1,18 @@
 package me.libraryaddict.librarys.Abilities;
 
+import org.bukkit.event.EventHandler;
+
 import me.libraryaddict.Hungergames.Abilities.Ninja;
+import me.libraryaddict.Hungergames.Events.GameStartEvent;
 
 public class Werewolf extends Ninja {
     public String[] potionEffectsDuringDay = new String[] { "WEAKNESS 0" };
     public String[] potionEffectsDuringNight = new String[] { "SPEED 0", "INCREASE_DAMAGE 0" };
+
+    @EventHandler
+    public void gameStartEvent(GameStartEvent event) {
+        super.potionEffectsDuringDay = potionEffectsDuringDay;
+        super.potionEffectsDuringNight = potionEffectsDuringNight;
+        super.gameStartEvent(event);
+    }
 }
